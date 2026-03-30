34e70614541a0f4bfb3871ebd35cb9a2e36ce4b8
pravinmajhi <83201958+pravinmajhi@users.noreply.github.com>
2026-03-30 20:47:48 +0530

    Load SECRET_KEY and DEBUG from environment variables

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+import os
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG') == 'True'
